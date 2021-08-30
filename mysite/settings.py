a12b120884cfeeedb4759029afde4b36abf916a5
Sooryakantha G <ssooryakanthnaik@gmail.com>
2021-08-30 19:35:46 +0530

    Blog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-scr*qc-o7hp4$uwmde11^3o4_-@gy%t()iv7+d1$+q0t66a^nz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogbysooryakanth.herokuapp.com','127.0.0.1']
 
 
 # Application definition
