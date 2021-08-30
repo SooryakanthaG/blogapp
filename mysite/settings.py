ffd34a8a285141321af6452b464ddf0119a18740
Sooryakantha G <ssooryakanthnaik@gmail.com>
2021-08-30 09:36:08 +0530

    Blog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ssooryakanthnaik@gmail.com'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'ssooryakanthnaik@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+EMAIL_BACKEND	=	'django.core.mail.backends.console.EmailBackend'
